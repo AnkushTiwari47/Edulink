997141453b07391fa2f0733b0b783492dc39f603
Ankush Tiwari <at0017876@gmail.com>
2026-06-23 05:50:49 +0530

    checking importart hardcodeded codes before deployment

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,10 @@ app.use("/chat", chatRoutes);
  
 const server = http.createServer(app);
 
-initSocket(server);   
+initSocket(server); 
+const PORT = process.env.PORT || 3000;
 
-server.listen(process.env.port || 3000, ()=>{  
-console.log("Server running on port 3000");
-});
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});  
+
