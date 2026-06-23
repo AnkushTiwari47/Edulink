be935d7436668b84385594baac5b4f02ed689716
Ankush Tiwari <at0017876@gmail.com>
2026-06-23 07:01:34 +0530

    fixing backend routing issue

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,11 @@ app.use("/connections", connectionRoute);
 app.use("/chat", chatRoutes); 
  
 const server = http.createServer(app);
-
+ 
 initSocket(server); 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`); 
 });  
 
